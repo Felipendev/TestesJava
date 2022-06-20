a3ea67966e4106c61716641bd1011eaba1bda2d7
Felipendev <felipe.psic@outlook.com.br>
2022-06-20 14:54:00 -0300

    Add exemplo de teste com Exception

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -13,9 +13,8 @@ class BonusServiceTest {
     @Test
     void bonusDeveriaSerZeroParaFuncionariosComSalarioMuitoAlto() {
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Felipe", LocalDate.now(), new BigDecimal(25000)));
-
-        assertEquals(new BigDecimal("0.00"), bonus);
+        assertThrows(IllegalArgumentException.class, ()->
+                service.calcularBonus(new Funcionario("Felipe", LocalDate.now(), new BigDecimal(25000))));
     }
 
     @Test
